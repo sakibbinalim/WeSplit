418bd39adc03d0d1a72091956f6b9503947ec396
Sakib Bin Alim <sakib.alim@bjitgroup.com>
2024-05-29 12:12:38 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sakib Bin Alim on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
